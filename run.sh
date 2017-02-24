baecae4a775e17ff9f7e23d108df4e5038dc6793
Darryl Bidulock <darryl@arterys.com>
2017-02-24 10:59:45 -0700

    Add a default url for joining a meeting

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 DOCKERIMG=${DOCKERIMG:-mbessler/ff32-webex}
 
+default_url=https://www.webex.com/go/attend-meeting
+url=${1-$default_url}
+
 xhost +local:webex-$(id -un)
-docker run --rm --hostname=webex-$(id -un) -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v /dev/snd:/dev/snd -v $HOME/ff32webex:/webex ${DOCKERIMG} $*
+docker run --rm --hostname=webex-$(id -un) -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v /dev/snd:/dev/snd -v $HOME/ff32webex:/webex ${DOCKERIMG} $url
